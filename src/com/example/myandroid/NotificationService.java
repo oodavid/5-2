cf7f5646ce96d4c51122fb75abc36d0c59dde4a0
David King <david@oodavid.com>
2013-08-26 20:30:20 +0100

    Added Settings Activity (choose Day) and general tidy

--- src/com/example/myandroid/NotificationService.java
+++ src/com/example/myandroid/NotificationService.java
@@ -1,30 +1,21 @@
 package com.example.myandroid;
 
-import java.util.Calendar;
- 
-import android.app.Activity;
-import android.app.AlarmManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Window;
- 
-public class AlarmMainActivity extends Activity 
-{
+import android.util.Log;
+
+public class NotificationService extends Service {
+
 	// Id of our Notification
 	private static final int HELLO_ID = 1;
  
-	@Override
-	public void onCreate(Bundle savedInstanceState) 
-	{
-		// Set the content of the window
-		// NB: R.layout.main = /res/layout/main.xml
-		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.main);
+	@Override public int onStartCommand(Intent intent, int flags, int startId) {
 
 		//
 		// NOTIFICATION
@@ -40,7 +31,7 @@ public class AlarmMainActivity extends Activity
 			// Create a notification Object
 			// NB: The tickertext appears in the notification BAR when first added
 			int				icon			= R.drawable.icon;
-			CharSequence	tickerText		= "Hello";
+			CharSequence	tickerText		= "Diet Day!";
 			long			when			= System.currentTimeMillis();
 			Notification	notification	= new Notification(icon, tickerText, when);
 
@@ -48,7 +39,7 @@ public class AlarmMainActivity extends Activity
 			// NB: We also assign an intent which is fired when we click the notification
 			CharSequence	contentTitle		= "My notification";
 			CharSequence	contentText			= "Hello World!";
-			Intent			notificationIntent	= new Intent(this, AlarmReceiverActivity.class);
+			Intent			notificationIntent	= new Intent(this, SettingsActivity.class);
 			PendingIntent	contentIntent		= PendingIntent.getActivity(this, 0, notificationIntent, 0);
 			notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
 
@@ -58,21 +49,23 @@ public class AlarmMainActivity extends Activity
 			// Send the notification
 			nm.notify(HELLO_ID, notification);
 
-		//
-		// AN ALARM / TIMER
-		//
-			/*
-
-			// Create an offset from the current time in which the alarm will go off.
-			Calendar cal = Calendar.getInstance();
-			cal.add(Calendar.SECOND, 5);
-
-			// Create a new PendingIntent and add it to the AlarmManager
-			Intent			intent			= new Intent(this, AlarmReceiverActivity.class);
-			PendingIntent	pendingIntent	= PendingIntent.getActivity(this, 12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-			AlarmManager	am				= (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
-			am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
-
-			*/
+		// There are a few options as to what to return
+		// I'm not 100% that this is the correct one (for lack of understanding)
+		return Service.START_NOT_STICKY;
 	}
-}
+
+	//
+	// Not entirely sure why we need these parts
+	// I've stripped it down to the smallest amount of codez that work
+	//
+    public class LocalBinder extends Binder {
+        NotificationService getService() {
+            return NotificationService.this;
+        }
+    }
+	private final IBinder mBinder = new LocalBinder();
+	@Override public IBinder onBind(Intent intent) {
+		return mBinder;
+	}
+
+} 
--- src/com/example/myandroid/SettingsActivity.java
+++ src/com/example/myandroid/SettingsActivity.java
@@ -1,9 +1,74 @@
+/**
+ * 5-2 > Settings
+ *
+ *		Simply allows the user to select the days of the week
+ *		that they are dieting. This is the default activity that
+ *		is called when running the app OR clicking a notification.
+ *
+ * @author		David "oodavid" King
+ * @copyright	Copyright (c) 2013 +
+ */
+
 package com.example.myandroid;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.widget.CheckBox;
+import android.widget.Button;
+import android.util.Log;
 
-/*
-	RUN WHEN YOU CLICK NOTIFICATION
-	RUN WHEN YOU RUN APP
+public class SettingsActivity extends Activity {
 
-		SELECT DIET-DAY(S)
-		SAVE
-*/
+	@Override public void onCreate(Bundle savedInstanceState) {
+
+		// Render a window with the main layout
+		// NB: R.layout.main = /res/layout/main.xml
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
+
+		// Which days are we dieting?
+		// NB: Defaults to monday and thursday
+		SharedPreferences settings	= getSharedPreferences("MyPrefs", 0);
+		CheckBox checkbox_monday	= (CheckBox) findViewById(R.id.checkbox_monday);	checkbox_monday.setChecked		(settings.getBoolean("monday",		true));
+		CheckBox checkbox_tuesday	= (CheckBox) findViewById(R.id.checkbox_tuesday);	checkbox_tuesday.setChecked		(settings.getBoolean("tuesday",		false));
+		CheckBox checkbox_wednesday	= (CheckBox) findViewById(R.id.checkbox_wednesday);	checkbox_wednesday.setChecked	(settings.getBoolean("wednesday",	false));
+		CheckBox checkbox_thursday	= (CheckBox) findViewById(R.id.checkbox_thursday);	checkbox_thursday.setChecked	(settings.getBoolean("thursday",	true));
+		CheckBox checkbox_friday	= (CheckBox) findViewById(R.id.checkbox_friday);	checkbox_friday.setChecked		(settings.getBoolean("friday",		false));
+		CheckBox checkbox_saturday	= (CheckBox) findViewById(R.id.checkbox_saturday);	checkbox_saturday.setChecked	(settings.getBoolean("saturday",	false));
+		CheckBox checkbox_sunday	= (CheckBox) findViewById(R.id.checkbox_sunday);	checkbox_sunday.setChecked		(settings.getBoolean("sunday",		false));
+
+	}
+
+	// Saves the settings
+	public void onBtnClicked(View v){
+		
+		// I'm not sure how to keep the settings and checkboxes in scope so I'm re-declaring them....
+		SharedPreferences			settings		= getSharedPreferences("MyPrefs", 0);
+		SharedPreferences.Editor	settingsEditor	= settings.edit();
+
+		// And save...
+		CheckBox checkbox_monday	= (CheckBox) findViewById(R.id.checkbox_monday);	settingsEditor.putBoolean("monday",		checkbox_monday.isChecked()).commit();
+		CheckBox checkbox_tuesday	= (CheckBox) findViewById(R.id.checkbox_tuesday);	settingsEditor.putBoolean("tuesday",	checkbox_tuesday.isChecked()).commit();
+		CheckBox checkbox_wednesday	= (CheckBox) findViewById(R.id.checkbox_wednesday);	settingsEditor.putBoolean("wednesday",	checkbox_wednesday.isChecked()).commit();
+		CheckBox checkbox_thursday	= (CheckBox) findViewById(R.id.checkbox_thursday);	settingsEditor.putBoolean("thursday",	checkbox_thursday.isChecked()).commit();
+		CheckBox checkbox_friday	= (CheckBox) findViewById(R.id.checkbox_friday);	settingsEditor.putBoolean("friday",		checkbox_friday.isChecked()).commit();
+		CheckBox checkbox_saturday	= (CheckBox) findViewById(R.id.checkbox_saturday);	settingsEditor.putBoolean("saturday",	checkbox_saturday.isChecked()).commit();
+		CheckBox checkbox_sunday	= (CheckBox) findViewById(R.id.checkbox_sunday);	settingsEditor.putBoolean("sunday",		checkbox_sunday.isChecked()).commit();
+
+		// Call the Notification Activity (this will "refresh" the notification)
+		// Start and trigger the notificaation service
+		Intent i = new Intent(getBaseContext(), NotificationService.class);
+		startService(i);
+		finish();
+
+	}
+
+}
